1e504033d13fd9fd221afc9bf5dd3aa61c2dc8e7
yrtxc1986 <keso.haneyou@gmail.com>
2023-03-22 12:23:42 +0800

    First click check ready

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,6 @@
 // Imports
 import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 
 // To Test
 import App from "../App";
@@ -12,7 +13,10 @@ test("Renders main page correctly", async () => {
   // Pre Expecations
   expect(buttonCount.innerHTML).toBe("count is 0");
   // Init
+  await user.click(buttonCount);
+  await user.click(buttonCount);
 
   // Post Expectations
-  expect(true).toBeTruthy();
+  //   expect(true).toBeTruthy();
+  expect(buttonCount.innerHTML).toBe("count is 2");
 });
